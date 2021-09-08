a68f8ddaeafd9c319a592c55b1bf0d8da9b2054b
Federico Panella <federico.e.panella@gmail.com>
2021-09-08 13:14:01 -0300

    CHECK LISTO PARA RESOLVER

--- app.js
+++ app.js
@@ -2,9 +2,6 @@
 var router = require('./routes/index.js')
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var characters = require('./routes/index.js')
-var model = require('./models/model.js')
 module.exports = app; // Esto es solo para testear mas facil
 
 // Acuerdense de agregar su router o cualquier middleware que necesiten aca
--- models/model.js
+++ models/model.js
@@ -1,98 +1,3 @@
-/* 'use strict';
-
-var characters = []; // arreglo de personajes de cada familia {'Homero', 36, "Simpsons"}
-
-var families = []; //arreglo de familias ['Simpsons'[{'Homero', 36, "Simpsons"},{'Bart', 10, "Simpsons"}], 'Gorgory'[{'Rafa', 10, "Gorgory"}]]
-
-
-//hola fede
-
-module.exports = {
-  reset: function () {
-    // No es necesario modificar esta función (Ya está completa)
-    characters = [];
-    families = [];
-  },
-  // ==== COMPLETEN LAS SIGUIENTES FUNCIONES (vean los test de `model.js`) =====
-  listCharacter: function (family, pluckName) {
-    // Devuelve un arreglo con todos los personajes
-    // Si recibe un nombre de familia como parámetro debería filtrar solo los personajes de ella
-    // Si recibe un segundo parámetro en true debe devolver únicamente los nombres de los personajes
-    let names = [];
-    if (family && pluckName) {  //SI RECIBE UN SEGUNDO PARAMETRO FILTRO EL ARRAY DE CHARACTERS Y DEVUELVO NAMES CON LOS NOMBRES DE LOS PERSONAJES
-      characters.filter(character => {
-        if (character.familyId === families.indexOf(family)+1) { //Aca paso el id porque el character no tiene una prop family
-          names.push(character.name)
-        }
-      })
-      return names;
-    } else if (family) { //sino le mando los personajes de family que matcheen
-      characters.filter(character => {
-        if (character.familyId === families.indexOf(family)+1) {
-          names.push(character)
-        }
-      })
-      return names;
-    } else {
-
-      return characters;
-    }
-
-  },
-  addFamily: function (name) {
-    // Agrega el apellido de una nueva familia verificando que no exista
-    // Debe retornar el nombre de la familia agregado o existente
-    if (!families.includes(name)) {
-      families.push(name)
-      return name;
-    }
-  return families;
-  },
-  listFamilies: function () {
-    // Devuelve un arreglo con todas las familias
-    return families;
-  },
-  addCharacter: function (name, age, family) {
-    // Agrega un nuevo personaje, inicialmente sus frases (quotes) deben estar "vacias"
-    // Adicionalmente va a ser necesario guardar el número de familia y no su nombre !!!!!
-    // El número de familia debe empezar desde 1 y no desde 0.
-    // Debe retornar el personaje creado
-    let familyId = families.indexOf(family) + 1
-    let quotes = [];
-    let newCharacter = {
-      name,
-      age,
-      quotes,
-      familyId
-    }
-    if (families.includes(family)) {
-      characters.push(newCharacter)
-      return newCharacter;
-    }
-  },
-  addQuote: function (name, quote) {
-    // Agrega una nueva frase a un personaje en particular con el formato:
-    // {text: "Este es el texto de la frase", season: 3} // 
-    // QUOTE === OBJETOS CON PROPIEDADES TEXT Y SEASON
-    //QUOTES ES UN ARREGLO SETEADO COMO PROPIEDAD DEL CHARACTER
-    let filterCharacter = characters.filter (character => character.name === name);
-    if (quote.text){
-      if (!quote.season) {
-     quote.season = false;
- }
- filterCharacter[0].quotes.push(quote);
-    }
-  },
-
-  showQuotes: function (name) {
-    // Devuelve todas las frases de un personaje en particular
-    let filterCharacter2 = characters.filter (character => character.name === name);
-    if (filterCharacter2.length>0){ 
-    return filterCharacter2[0].quotes;
-}
-return filterCharacter2;
-  }
-}; */
 
 ///==========================================================================///
 ///===============================HENRY-POTTER===============================///
@@ -100,17 +5,10 @@ return filterCharacter2;
 
 'use strict';
 
-var characters = []; // arreglo de personajes de cada familia {'Homero', 36, "Simpsons"}
+var characters = []; 
 
-var houses = []; //arreglo de familias ['Simpsons'[{'Homero', 36, "Simpsons"},{'Bart', 10, "Simpsons"}], 'Gorgory'[{'Rafa', 10, "Gorgory"}]]
+var houses = []; 
 
-
-//hola fede 
-
-// ruta que liste a la gente mágica solamente
-// ruta que traiga solo a los personajes que tengan spells
-// hacer el addWand
-// hacer el showWand
 
 
 module.exports = {
@@ -124,39 +22,16 @@ module.exports = {
     // Devuelve un arreglo con todos los personajes
     // Si recibe un nombre de house como parámetro debería filtrar solo los personajes de ella
     // Si recibe un segundo parámetro en true debe devolver únicamente los nombres y apellidos de los personajes
-    let names = [];
-    if (house && fullName) { 
-      characters.filter(character => {
-        if (character.houseId === houses.indexOf(house) + 1) { 
-          names.push(character.name + " " + character.lastName)
-        }
-      })
-      return names;
-    } else if (house) { 
-      characters.filter(character => {
-        if (character.houseId === houses.indexOf(house) + 1) {
-          names.push(character)
-        }
-      })
-      return names;
-    } else {
-
-      return characters;
-    }
-
+    
   },
+
   addHouse: function (house) {
     // Agrega el nombre de una nueva casa verificando que no exista
     // Debe retornar el nombre de la casa agregado o existente
-    if (!houses.includes(house)) {
-      houses.push(house)
-      return house;
-    }
-    return house;
   },
+
   listHouses: function () {
     // Devuelve un arreglo con todas las casas
-    return houses;
   },
 
   addCharacter: function (name, lastName, house, dateOfBirth, isMuggle) {
@@ -170,26 +45,6 @@ module.exports = {
     // no debe agregarse el personaje al arreglo de personajes y debe devolver un objeto con un mensaje de error,
     // (mirar en los tests)
     // Si la casa existe y el personaje es agregado con éxito debe retornar el personaje creado
-    let wand = {}
-    let houseId = houses.indexOf(house) + 1
-    let spells = [];
-    let yearOfBirth = Number(dateOfBirth.slice(-4))
-    let newCharacter = {
-      name,
-      houseId,
-      lastName,
-      dateOfBirth,
-      yearOfBirth,
-      isMuggle,
-      wand,
-      spells
-    }
-    if (houses.includes(house)) {
-      characters.push(newCharacter)
-      return newCharacter;
-    }else{
-      return {msg: 'La casa ingresada no existe'}
-    }
   },
 
   addSpell: function (name, id, spellName, description) {
@@ -199,28 +54,12 @@ module.exports = {
     //{id: id, spellName: spellName, description: description}
     // una vez agregado el hechizo debe retornar un objeto con un mensaje de éxito (ver el test de las rutas)
     // Si no se le pasa id, spellName o description no agrega el hechizo al personaje
-
-    if(!id || !spellName || !description) return
-
-    let filterCharacter = characters.filter(character => character.name === name);
-    if (!description) description = false
-    filterCharacter[0].spells.push({
-      id,
-      spellName,
-      description
-    });
-    return {msg: "Hechizo agregado correctamente"};
   },
 
   showSpells: function (name) {
     // Devuelve todos los hechizos de un personaje en particular
     // Si no encuentra al personaje que matchee con el nombre recibido por parámetros
     // devuelve un arreglo vacío
-    let filterCharacter2 = characters.filter(character => character.name === name);
-    if (filterCharacter2.length > 0) {
-      return filterCharacter2[0].spells;
-    }
-    return filterCharacter2;
   },
 
   addWand: function (name, wood, core, length) {
@@ -231,29 +70,12 @@ module.exports = {
     // "Ya existe una varita para este personaje"
     // caso contrario debe agregar a la propiedad wand del personaje un objeto de la siguiente forma
     // {wood: wood, core: core, length: length}
-    let filterCharacter = characters.filter(character => character.name === name);
-    if (filterCharacter.length === 0) return filterCharacter
-    if (!filterCharacter[0].wand.wood) {
-      filterCharacter[0].wand= {
-        wood,
-        core,
-        length
-      };
-      return "varita agregada correctamente"
-    } else {
-      return "Ya existe una varita para este personaje"
-    }
   },
   
   showWand: function (name) {
     // Devuelve la varita de un personaje en particular
     // Si no encuentra al personaje que matchee con el nombre recibido por parámetros devuelve un arreglo vacío
     // Si el personaje en cuestión no tiene varita devuelve el string "el personaje no tiene varita"
-    let filterCharacter2 = characters.filter(character => character.name === name);
-    if (filterCharacter2.length > 0) {
-    return filterCharacter2[0].wand.wood ? filterCharacter2[0].wand : "el personaje no tiene varita";
-    }
-    return filterCharacter2;
   },
 
 
